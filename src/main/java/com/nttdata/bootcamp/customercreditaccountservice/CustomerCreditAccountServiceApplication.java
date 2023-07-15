a87cf8010e7fef3ed0e87756d1752fd20798f78e
consorcioCAP1 <omar.dcruz12@gmail.com>
2023-07-15 14:23:35 -0500

    primer commit del customer-credit-account-service

---
+++ src/main/java/com/nttdata/bootcamp/customercreditaccountservice/CustomerCreditAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.customercreditaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCreditAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerCreditAccountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/customercreditaccountservice/CustomerCreditAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.customercreditaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCreditAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
